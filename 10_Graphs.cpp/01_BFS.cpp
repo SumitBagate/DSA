8ff56947860f5ed242c4917fb3f247d4e16f8348
sumit <sumitbagate3@gmail.com>
2026-02-25 12:19:56 +0530

    BFS

---
+++ 10_Graphs.cpp/01_BFS.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+void BFS(int start, vector<vector<int>>& adj, int V)
+{
+   
+    vector<bool> visited(V+1, false);
+    queue<int> q;
+
+    visited[start]=true;
+    q.push(start);
+
+    while(!q.empty()){
+         int  node = q.front();
+         q.pop();
+
+         cout<< node << " ";
+
+         for(int neighbor: adj[node])
+         {
+            if(!visited[neighbor])
+            {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+         }
+    }
+
+}
+
+int main()
+{  
+    int V,E;//vcertices and edges
+    cin>>V>>E;
+
+    vector<vector<int>> adj(V+1);
+
+    for(int i = 0; i < E ; i++){
+        int u, v;
+        cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);// remove for directed  graph
+    }
+    
+    int start;
+    cin>>start;
+
+    BFS(start,adj, V);
+
+    return 0;
+}
